e953829b0eb3bda50c5e7cb95de7b38ee852a478
JP <Jahanavee.Potturu1@ibm.com>
2022-03-14 23:22:36 -0400

    update to use manifest

---
+++ modules/.terraform.lock.hcl
@@ -0,0 +1,34 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/kubernetes" {
+  version = "2.8.0"
+  hashes = [
+    "h1:tfU8BStZIt2d6KIGTRNjWb09zeVzh3UFGNRGVgFce+A=",
+    "zh:0cf42c17c05ae5f0f5eb4b2c375dd2068960b97392e50823e47b2cee7b5e01be",
+    "zh:29e3751eceae92c7400a17fe3a5394ed761627bcadfda66e7ac91d6485c37927",
+    "zh:2d95584504c651e1e2e49fbb5fae1736e32a505102c3dbd2c319b26884a7d3d5",
+    "zh:4a5f1d915c19e7c7b4f04d7d68f82db2c872dad75b9e6f33a6ddce43aa160405",
+    "zh:4b959187fd2c884a4c6606e1c4edc7b506ec4cadb2742831f37aca1463eb349d",
+    "zh:5e76a2b81c93d9904d50c2a703845f79d2b080c2f87c07ef8f168592033d638f",
+    "zh:c5aa21a7168f96afa4b4776cbd7eefd3e1f47d48430dce75c7f761f2d2fac77b",
+    "zh:d45e8bd98fc6752ea087e744efdafb209e7ec5a4224f9affee0a24fb51d26bb9",
+    "zh:d4739255076ed7f3ac2a06aef89e8e48a87667f3e470c514ce2185c0569cc1fb",
+    "zh:dbd2f11529a422ffd17040a70c0cc2802b7f1be2499e976dc22f1138d022b1b4",
+    "zh:dbd5357082b2485bb9978bce5b6d508d6b431d15c53bfa1fcc2781131826b5d8",
+  ]
+}
+
+provider "registry.terraform.io/ibm-cloud/ibm" {
+  version     = "1.39.2"
+  constraints = ">= 1.36.0"
+  hashes = [
+    "h1:xfKbG4OG4ZDjE2zkFH3KNWHt4cP3IyKelZjERakuz2o=",
+    "zh:29806d1cd31fc6728ee68bc6da49b9c064f83d37d8facb8ec900a30d93c7a466",
+    "zh:47b6d8d4d85b3906acf88d152b4855fe78882221cb0c6a8fa0fdfb412af2dfbc",
+    "zh:7f9fdcbec8fa101ab5f5bb01146763498d983a230bc5d3c6aa709275fc2ccb13",
+    "zh:b046a4f436babe400525fa77610ba2313d5aef72a797003e37dd42e1098f623e",
+    "zh:e26a9dec9be6f6dc43277058268b50b9c560f108c773a6d4c4af8d1f40309393",
+    "zh:ec4e8ecdd68989c327d8ebfaf7ff620b0b9c36e74b90f3a4e1ec96c8672f7974",
+  ]
+}
---
+++ modules/monitoring.tf
@@ -0,0 +1,161 @@
+resource "kubernetes_manifest" "namespace_my_grafana_operator" {
+    depends_on = [kubernetes_manifest.subscription_openshift_logging_cluster_logging]   
+  manifest = {
+    "apiVersion" = "v1"
+    "kind" = "Namespace"
+    "metadata" = {
+      "annotations" = {
+        "openshift.io/node-selector" = ""
+      }
+      "labels" = {
+        "openshift.io/cluster-monitoring" = "true"
+      }
+      "name" = "my-grafana-operator"
+    }
+  }
+}
+
+resource "kubernetes_manifest" "configmap_openshift_monitoring_cluster_monitoring_config" {
+    depends_on = [kubernetes_manifest.namespace_my_grafana_operator]
+   
+  manifest = {
+    "apiVersion" = "v1"
+    "data" = {
+      "config.yaml" = <<-EOT
+      enableUserWorkload: true
+      prometheusOperator:
+        tolerations:
+        - key: "logging-monitoring"
+          operator: "Equal"
+          value: "node"
+          effect: "NoExecute"
+      prometheusK8s: 
+        retention: 1y
+        volumeClaimTemplate:
+          spec:
+            storageClassName: ibmc-vpc-block-retain-general-purpose
+            volumeMode: Filesystem
+            resources:
+               requests:
+                 storage: 100Gi
+        tolerations:
+        - key: "logging-monitoring"
+          operator: "Equal"
+          value: "node"
+          effect: "NoExecute"
+      alertmanagerMain:
+        tolerations:
+        - key: "logging-monitoring"
+          operator: "Equal"
+          value: "node"
+          effect: "NoExecute"
+      kubeStateMetrics:
+        tolerations:
+        - key: "logging-monitoring"
+          operator: "Equal"
+          value: "node"
+          effect: "NoExecute"
+      openshiftStateMetrics:
+        tolerations:
+        - key: "logging-monitoring"
+          operator: "Equal"
+          value: "node"
+          effect: "NoExecute"
+      telemeterClient:
+        tolerations:
+        - key: "logging-monitoring"
+          operator: "Equal"
+          value: "node"
+          effect: "NoExecute"
+      k8sPrometheusAdapter:
+        tolerations:
+        - key: "logging-monitoring"
+          operator: "Equal"
+          value: "node"
+          effect: "NoExecute"
+      thanosQuerier:
+        tolerations:
+        - key: "logging-monitoring"
+          operator: "Equal"
+          value: "node"
+          effect: "NoExecute"
+      
+      EOT
+    }
+    "kind" = "ConfigMap"
+    "metadata" = {
+      "name" = "cluster-monitoring-config"
+      "namespace" = "openshift-monitoring"
+    }
+  }
+}
+
+resource "kubernetes_manifest" "configmap_openshift_user_workload_monitoring_user_workload_monitoring_config" {
+    depends_on = [kubernetes_manifest.configmap_openshift_monitoring_cluster_monitoring_config]
+
+  manifest = {
+    "apiVersion" = "v1"
+    "data" = {
+      "config.yaml" = <<-EOT
+      prometheus:
+        retention: 1y
+        volumeClaimTemplate:
+          spec:
+            storageClassName: ibmc-vpc-block-retain-general-purpose
+            volumeMode: Filesystem
+            resources:
+              requests:
+                storage: 100Gi
+        tolerations:
+        - key: "logging-monitoring"
+          operator: "Equal"
+          value: "node"
+          effect: "NoExecute"
+      
+      EOT
+    }
+    "kind" = "ConfigMap"
+    "metadata" = {
+      "name" = "user-workload-monitoring-config"
+      "namespace" = "openshift-user-workload-monitoring"
+    }
+  }
+}
+
+resource "kubernetes_manifest" "operatorgroup_my_grafana_operator_my_grafana_operator" {
+    depends_on = [kubernetes_manifest.configmap_openshift_user_workload_monitoring_user_workload_monitoring_config]
+
+  manifest = {
+    "apiVersion" = "operators.coreos.com/v1"
+    "kind" = "OperatorGroup"
+    "metadata" = {
+      "name" = "my-grafana-operator"
+      "namespace" = "my-grafana-operator"
+    }
+    "spec" = {
+      "targetNamespaces" = [
+        "my-grafana-operator",
+      ]
+    }
+  }
+}
+
+resource "kubernetes_manifest" "subscription_my_grafana_operator_my_grafana_operator" {
+    depends_on = [kubernetes_manifest.operatorgroup_my_grafana_operator_my_grafana_operator]
+
+  manifest = {
+    "apiVersion" = "operators.coreos.com/v1alpha1"
+    "kind" = "Subscription"
+    "metadata" = {
+      "name" = "my-grafana-operator"
+      "namespace" = "my-grafana-operator"
+    }
+    "spec" = {
+      "channel" = "v4"
+      "name" = "grafana-operator"
+      "source" = "community-operators"
+      "sourceNamespace" = "openshift-marketplace"
+    }
+  }
+}
+
